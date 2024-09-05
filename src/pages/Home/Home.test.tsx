48154c168b6c5829215781332d23a1056d3ec09a
Luis Jimenez <luisjc140992@gmail.com>
2024-09-04 22:10:49 -0600

    fix: remove unecessary cleanup

--- src/pages/Home/Home.test.tsx
+++ src/pages/Home/Home.test.tsx
@@ -1,17 +1,15 @@
-import { render, waitFor, cleanup } from '@testing-library/react';
+import { render, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import Home from '.';
-
-afterEach(cleanup)
 
 test('Check suspense and lazy loading component', async () => {
     // Render the component
     const { getByText } = render(<Home />);
 
     const loadingText = getByText(/Component is loading, please wait/)
-
     expect(loadingText).toBeInTheDocument()
 
+    // Render components after lazyloading
     const firtsText = await waitFor(() => getByText(/accusamus beatae ad facilis cum similique qui sunt/))
     expect(firtsText).toBeInTheDocument()
 });
